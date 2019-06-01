cc38a15ab1df74b70c115edcee224dc9dfb7f399
Rodrigo Andrade <rodrigopires.profissional@gmail.com>
2019-06-01 13:38:01 -0300

    Alteracao

--- Listas de Exercicio/Lista 13/Exercicio3.c
+++ Listas de Exercicio/Lista 13/Exercicio3.c
@@ -3,6 +3,7 @@
  * nome do famoso mais velho ou se as idades são iguais. O programa deve parar de ler
  * os dados se for digitado fim(maiúsculo ou minúsculo) no nome
  * */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -27,6 +28,11 @@ int main(int argv, char *argc[])
   //Indice para controlar o vetor:
   short indice = 0; 
 
+  //Variavel igual se o valor for igual a 1, significa que as idades são iguais:
+  int igual = 0;
+
+  const int tam = 2;
+
   while(1)
   {
     printf("Nome: ");
@@ -43,25 +49,55 @@ int main(int argv, char *argc[])
 
     indice++;
 
-    if(indice == 2) 
+    //Index vai sempre retornar o Maior:
+    int index, Padrao = 0;
+
+    if(indice == tam) 
     {
+      
+      int MaiorIdade = Pessoa[0].Idade;
+
+      for(index = 1; index < tam; index++)
+      {
+        if(Pessoa[index].Idade > MaiorIdade)
+          MaiorIdade = Pessoa[index].Idade;
+        else
+          if(Pessoa[index].Idade == MaiorIdade)
+            igual = 1;
+          else
+            //Indice 0 é o maior
+            Padrao = 1;
+      }
+
       //Reiniciando o valor do Indice:
       indice = 0;
 
-      if(Pessoa[0].Idade > Pessoa[1].Idade)
-      {
-        printf("%s é mais velho que %s\n", Pessoa[0].Nome, Pessoa[1].Nome);
-      }
-      else if(Pessoa[1].Idade > Pessoa[0].Idade)
-      {
-        printf("%s é mais velho que %s\n", Pessoa[1].Nome, Pessoa[0].Nome);
-      }
-      else
-      {
-        printf("%s e %s têm a mesma idade\n", Pessoa[0].Nome, Pessoa[1].Nome);
-      }
-    }//Fim if
 
+    //Imprimir resultados: 
+    if(igual == 1)
+    {
+      printf("%s e ", Pessoa[0].Nome);
+      for(int i = 1; i <= tam-1; i++)
+      {
+        if((tam-1) > 2)
+          printf("%s, ", Pessoa[i].Nome);
+        else
+          printf("%s", Pessoa[i].Nome);
+      }
+      printf(" tem a mesma idade.\n");
+    }
+    else
+    {
+      if(tam > 2)
+        printf("%s tem a idade maior que todos.\n", Pessoa[index].Nome);
+      else
+        if(Padrao == 1)
+          printf("%s é mais velho que %s\n", Pessoa[0].Nome, Pessoa[1].Nome);
+        else
+          printf("%s é mais velho que %s\n", Pessoa[1].Nome, Pessoa[0].Nome);
+    }
+
+    }//Fim if que verifica se o indice == 2
     setbuf(stdin, NULL);
   }
 
