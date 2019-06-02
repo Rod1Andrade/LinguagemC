31b6aa08f4c726b6b60e3d89d03953b57af35df6
Rodrigo Andrade <rodrigopires.profissional@gmail.com>
2019-06-02 16:16:14 -0300

    Introdução - Alocação Dinâmica de memória

--- Boss-MainGate/AlocacaoDinamica/AlocacaoDinamica-1.c
+++ Boss-MainGate/AlocacaoDinamica/AlocacaoDinamica-1.c
@@ -1,12 +1,25 @@
 /**
  * Introdução a Alocação Dinâmica de memória:
  * */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argv, char *argc[])
 {
   
+  //Em alocação dinâmica de memória vamos fazer um processo 
+  //utilizando as bibliotecas padrão do C <stdlib.h>
+  //com as funções
+  //
+  //malloc
+  //calloc
+  //realloc
+  //free
+  //
+  //e o operador sizeof();
+  //
+  //Tranformaremos um ponteiro em um array.
 
   return 0;
 }
