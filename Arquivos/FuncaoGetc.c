436255e5b46313ff6e7124a6d5abe0daa0defc87
Rodrigo Andrade <rodrigopires.profissional@gmail.com>
2019-06-03 20:29:46 -0300

    Funcao getc

---
+++ Arquivos/FuncaoGetc.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+  /**
+   * Para ler caracteres de um arquivo usamos a função:
+   *
+   * fgetc()
+   *
+   * Forma Geral:
+   *
+   * int fgetc(FILE *file);
+   *
+   * Retornar:
+   * - Em caso de erro: a constante EOF
+   * - Em case de sucesso: o caracter
+   *
+   * */
+
+  FILE *file;
+  file = fopen("Arquivo.txt", "r");
+
+  if(file == NULL)
+  {
+    printf("Impossivel Abrir o arquivo.\n");
+    getchar();
+    exit(1);
+  }
+
+  printf("Lendo os dados dentro do Arquivo: \"Arquivo.txt\"\n");
+  char caracterLido = fgetc(file);
+  while(caracterLido != EOF)
+  {
+    printf("%c", caracterLido);
+    caracterLido = fgetc(file);
+  }
+
+  fclose(file);
+  
+  return 0;
+}
